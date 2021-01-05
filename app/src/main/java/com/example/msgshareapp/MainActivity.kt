cb076680de60694b2336b5354d256ca493d1af6b
Gureet Singh <gurjeetbhari@gmail.com>
2021-01-05 19:20:36 +0530

    Adding Button and Intent then send to another activity

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,8 +18,16 @@ class MainActivity : AppCompatActivity() {
             //code this sOL is a Lambda Expression
             Log.i("MainActivity", "Button was clicked !")
             //log.i will store the logs in logcat message as Button was clicked
-
             Toast.makeText(this, "This Button was clicked", Toast.LENGTH_SHORT).show()
         }
+
+        sendToAnother.setOnClickListener{
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message,Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
