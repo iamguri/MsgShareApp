2414d3f9243565e51a78437636bf2638b65bcc13
Gureet Singh <gurjeetbhari@gmail.com>
2021-01-05 18:50:04 +0530

    New Button with log and Toast

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.msgshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonShowToast.setOnClickListener{
+            //code this sOL is a Lambda Expression
+            Log.i("MainActivity", "Button was clicked !")
+            //log.i will store the logs in logcat message as Button was clicked
+
+            Toast.makeText(this, "This Button was clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
